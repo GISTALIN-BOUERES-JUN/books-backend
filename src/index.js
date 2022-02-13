8da0630385e411d73e6cc58272543ae4299e4658
Geraldo Boueres <geraldoboueres@gmail.com>
2022-02-13 13:31:06 -0400

    Post / GET

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 const express = require('express');
-
+const { v4: uuidv4 } = require('uuid');
 const app = express();
+
+app.use(express.json());
+
+/**
+ * name - string
+ * author - string
+ * year of publishing - date
+ * ISBN number - string
+ * id - uuid
+ */
+
+const books = [];
+
+app.post("/books", (request, response)=>{
+    const {name, author, ISBN, year} = request.body;
+    books.push({
+        name,
+        author,
+        ISBN,
+        id: uuidv4(),
+        year,
+    });
+
+    return response.status(201).send();
+
+})
+
+app.get("/books/:ISBN", (request, response)=>{
+    const {ISBN} = request.params;
+
+})
 
 app.listen(4000);
